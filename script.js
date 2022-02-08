a99eaf6fcdb1ba7c6a4505990baabb47c7dd5915
Celian Debethune <celian.debethune@gmail.com>
2022-02-08 12:21:19 +0100

    Enter Number

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttons = document.querySelectorAll("button");
-
+const result = document.querySelector(".result");
 //Populate the grid
 buttons.forEach((button) => (button.style.gridArea = button.classList[0]));
 
@@ -8,23 +8,32 @@ let left_value = 0;
 let right_value = 0;
 let operator = "+";
 
-function handleNumber() {}
+function handleNumber() {
+  right_value *= 10;
+  right_value += parseInt(this.innerText);
+  console.log(right_value);
+  display(right_value);
+}
 function handleOperation() {}
 function handleResult() {}
 function handleAC() {}
 function handleClear() {}
 
+function display(number) {
+  result.innerText = number;
+}
+
 //Add event listeners
 buttons.forEach((button) => {
   if (button.classList.contains("number")) {
-    button.addEventListener("click", handleNumber());
+    button.addEventListener("click", handleNumber);
   } else if (button.classList.contains("operation")) {
-    button.addEventListener("click", handleOperation());
+    button.addEventListener("click", handleOperation);
   } else if (button.classList.contains("equal")) {
-    button.addEventListener("click", handleResult());
+    button.addEventListener("click", handleResult);
   } else if (button.classList.contains("ac")) {
-    button.addEventListener("click", handleAC());
-  } else if (button.contains.contains("clear")) {
-    button.addEventListener("click", handleClear());
+    button.addEventListener("click", handleAC);
+  } else if (button.classList.contains("clear")) {
+    button.addEventListener("click", handleClear);
   }
 });
